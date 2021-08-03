dc3e6b42347c7b1fdaa93875eae44c460993eb0c
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-08-03 05:31:46 -0700

    start and reset functions

--- script.js
+++ script.js
@@ -10,5 +10,40 @@ window.onload = function () {
 
   startButton.onclick = function () {
     clearInterval(interval);
+    Interval = setInterval(startTimer, 10);
   };
+  stopButton.onclick = function () {
+    clearInterval(Interval);
+  };
+  restartButton.onclick = function () {
+    clearInterval(Interval);
+    tenths = "00";
+    seconds = "00";
+    appendTenths.innerHTML = tenths;
+    appendSeconds.innerHTML = seconds;
+  };
+
+  function startTimer() {
+    tenths++;
+
+    if (tenths <= 9) {
+      appendTenths.innerHTML = "0" + tenths;
+    }
+
+    if (tenths > 9) {
+      appendTenths.innerHTML = tenths;
+    }
+
+    if (tenths > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tenths = 0;
+      appendTenths.innerHTML = "0" + 0;
+    }
+
+    if (seconds > 9) {
+      appendSeconds.innerHTML = seconds;
+    }
+  }
 };
